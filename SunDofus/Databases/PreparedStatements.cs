12538c5e0aa7682eb930ce874ca02c73cfd0cc47
Sebastian Valle <s.v.h21@hotmail.com>
2013-10-19 22:26:26 -0500

    Forgot to add this file.

---
+++ SunDofus/Databases/PreparedStatements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunDofus.Databases
+{
+    enum Queries
+    {
+        InsertNewBank,
+        UpdateBank,
+
+    }
+
+    static class PreparedStatements
+    {
+        static Dictionary<Queries, string> QueryMap = new Dictionary<Queries, string>();
+
+        static PreparedStatements()
+        {
+            QueryMap.Add(Queries.InsertNewBank, "INSERT INTO banks (Owner, Kamas, Items) VALUES (@Owner, @Kamas, @Items)");
+            QueryMap.Add(Queries.UpdateBank, "UPDATE banks SET Owner=@Owner, Kamas=@Kamas, Items=@Items WHERE Owner=@Owner");
+        }
+
+        public static string GetQuery(Queries query)
+        {
+            return QueryMap[query];
+        }
+    }
+}
